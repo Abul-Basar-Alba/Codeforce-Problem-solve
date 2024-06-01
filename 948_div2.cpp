3c3a9533a1f80f2db778fe72fb7f960d84cc22c3
Abul Basar Alba <150958151+ABULBASAR36@users.noreply.github.com>
2024-06-02 00:31:01 +0600

    Update 948_div2.cpp

--- 948_div2.cpp
+++ 948_div2.cpp
@@ -28,104 +28,59 @@
 //
 
 
-//#include<bits/stdc++.h>
-//#define ll long long int
-//using namespace std;
-//
-//void solve()
-//{
-//    ll t;
-//    cin >> t;
-//    while (t--)
-//    {
-//        ll x;
-//        cin >> x;
-//        vector<ll> result;
-//
-//        while (x > 0)
-//        {
-//            if (x & 1)
-//            {
-//                if (x & 2)
-//                {
-//                    result.push_back(-1);
-//                    x = (x + 1) >> 1;
-//                }
-//               else
-//                {
-//                    result.push_back(1);
-//                    x >>= 1;
-//                }
-//            }
-//           else
-//            {
-//                result.push_back(0);
-//                x >>= 1;
-//            }
-//        }
-//
-//        cout << result.size() << endl;
-//        for (int val : result)
-//        {
-//            cout << val << " ";
-//        }
-//        cout << endl;
-//    }
-//}
-//
-//int main()
-//{
-//    ios::sync_with_stdio(false);
-//    cin.tie(0);
-//    solve();
-//    return 0;
-//}
-//
-//
-
-
-#include <iostream>
-#include <vector>
-#include <unordered_set>
-#include <algorithm>
-#include <numeric> // for gcd
-
+#include<bits/stdc++.h>
+#define ll long long int
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
+void solve()
+{
+   ll t;
+   cin >> t;
+   while (t--)
+   {
+       ll x;
+       cin >> x;
+       vector<ll> result;
 
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<long long> a(n);
-        unordered_set<long long> elementsSet;
+       while (x > 0)
+       {
+           if (x & 1)
+           {
+               if (x & 2)
+               {
+                   result.push_back(-1);
+                   x = (x + 1) >> 1;
+               }
+              else
+               {
+                   result.push_back(1);
+                   x >>= 1;
+               }
+           }
+          else
+           {
+               result.push_back(0);
+               x >>= 1;
+           }
+       }
 
-        for (int i = 0; i < n; ++i) {
-            cin >> a[i];
-            elementsSet.insert(a[i]);
-        }
-
-        // Find the maximum special subsequence length
-        int maxSpecialSubseqLength = 0;
-        for (int i = 0; i < n; ++i) {
-            long long currentLCM = a[i];
-            for (int j = i; j < n; ++j) {
-                currentLCM = (currentLCM * a[j]) / __gcd(currentLCM, a[j]);
-                if (currentLCM > 1e9) break;
-                if (elementsSet.find(currentLCM) == elementsSet.end()) {
-                    maxSpecialSubseqLength = max(maxSpecialSubseqLength, j - i + 1);
-                } else {
-                    break;
-                }
-            }
-        }
-
-        cout << maxSpecialSubseqLength << '\n';
-    }
-
-    return 0;
+       cout << result.size() << endl;
+       for (int val : result)
+       {
+           cout << val << " ";
+       }
+       cout << endl;
+   }
 }
+
+int main()
+{
+   ios::sync_with_stdio(false);
+   cin.tie(0);
+   solve();
+   return 0;
+}
+
+
+
+
