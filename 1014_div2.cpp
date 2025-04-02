acfe46dcd4c47f2e9d192726efebcb6b25c0af15
Abul_Basar <mdabulbasarbasar025@gmail.com>
2025-04-02 23:36:22 +0600

    C

---
+++ 1014_div2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin >> t;
+    while(t--) 
+    {
+        ll n;
+        cin >> n;
+        vector<ll> a(n);
+        for (int i = 0; i < n; ++i) 
+        {
+            cin >> a[i];
+        }
+
+        ll mx = *max_element(a.begin(), a.end());
+        ll mn = *min_element(a.begin(), a.end());
+        cout << (mx - mn) << "\n";
+    }
+    return 0;
+}
