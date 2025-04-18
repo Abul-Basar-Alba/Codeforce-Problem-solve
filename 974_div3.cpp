65f9ee29649c22da51d3a2a36be994cd7b50159f
Abul_Basar <mdabulbasarbasar025@gmail.com>
2025-04-19 00:13:20 +0600

    Update

--- 974_div3.cpp
+++ 974_div3.cpp
@@ -1,38 +1,65 @@
-#include <bits/stdc++.h>
-#define ll long long int
-using namespace std;
+// #include <bits/stdc++.h>
+// #define ll long long int
+// using namespace std;
 
-int main() 
+// int main() 
+// {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     ll t;
+//     cin >> t;
+//     while(t--) 
+//     {
+//         ll n, k;
+//         cin >> n >> k;
+//         vector<ll> a(n);
+//         for (int i = 0; i < n; ++i) 
+//         {
+//             cin >> a[i];
+//         }
+
+//         ll tem = 0, ans = 0;
+//         for(int i = 0; i < n; i++)
+//         {
+//             if(a[i] >= k)
+//             {
+//                 tem += a[i];
+//             }
+//             else if(a[i] == 0 && tem > 0)
+//             {
+//                 ans++;
+//                 tem--; 
+//             }
+//         }
+//         cout << ans << "\n";
+//     }   
+//     return 0;
+// }
+
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     ll t;
     cin >> t;
-    while(t--) 
+    while(t--)
     {
         ll n, k;
         cin >> n >> k;
-        vector<ll> a(n);
-        for (int i = 0; i < n; ++i) 
-        {
-            cin >> a[i];
-        }
+        
+        ll e = (n/2) - ((n-k)/2);
 
-        ll tem = 0, ans = 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(a[i] >= k)
-            {
-                tem += a[i];
-            }
-            else if(a[i] == 0 && tem > 0)
-            {
-                ans++;
-                tem--; 
-            }
-        }
-        cout << ans << "\n";
-    }   
+        if ((k & 1) == (e & 1))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
     return 0;
 }
