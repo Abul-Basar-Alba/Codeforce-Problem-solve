2fe4bb0cdb0e3f0734044385403e4705f93172bd
Abul-Basar-Alba <mdabulbasarbasar025@gmail.com>
2025-07-25 01:39:40 +0600

    Kotlin_9Solved

--- 921_div2B.cpp
+++ 921_div2B.cpp
@@ -1,23 +1,26 @@
-#include<bits/stdc++.h>
-#define ll ll int
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-int main()
-{
-    ll t;
-    cin>>t;
-    while(t--)
-    {
-        ll x,n;
-        cin>>x>>n;
-        ll ans=0;
-        //ll d=__gcd(x,n);
-        for(ll i=1;i<=x;i++)
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        ll x, n;
+        cin >> x >> n;
+        ll ans = 1;
+
+        for (ll i = 1; i * i <= x; ++i) 
         {
-            if(x/i>=n&&x%i==0)
-                ans=min(ans,i);
-             if(x/i<n)
-                break;
+            if (x % i == 0) 
+            {
+                if (x / i >= n) 
+                ans = max(ans, i);
+                if (x / (x / i) >= n) 
+                ans = max(ans, x / i);
+            }
         }
-        cout<<ans<<endl;
+
+        cout << ans << endl;
     }
 }
--- kotlin_9.cpp
+++ kotlin_9.cpp
@@ -8,22 +8,24 @@ int main() {
 
     ll t; 
     cin >> t;
-    while (t--) {
+    while (t--) 
+    {
         string s;
         cin >> s;
         int n = s.size();
 
-        // Try every possible split point i: prefix = s[0..i-1], suffix = s[i..n-1]
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) 
+        {
             string prefix = s.substr(0, i);
             string suffix = s.substr(i);
 
-            // Check suffix: non-empty, all digits, no leading zero
             if (suffix[0] == '0') 
                 continue;
             bool allDigits = true;
-            for (char c : suffix) {
-                if (!isdigit(c)) {
+            for (char c : suffix) 
+            {
+                if (!isdigit(c)) 
+                {
                     allDigits = false;
                     break;
                 }
@@ -31,10 +33,11 @@ int main() {
             if (!allDigits) 
                 continue;
 
-            // Check prefix: must contain at least one lowercase letter
             bool hasLetter = false;
-            for (char c : prefix) {
-                if (c >= 'a' && c <= 'z') {
+            for (char c : prefix) 
+            {
+                if (c >= 'a' && c <= 'z') 
+                {
                     hasLetter = true;
                     break;
                 }
@@ -42,7 +45,6 @@ int main() {
             if (!hasLetter) 
                 continue;
 
-            // Found a valid split!
             cout << prefix << "\n";
             break;
         }
